20ae33f9c80e900d6fd2dfb7fa5a6d47b40bf35e
Samuel Claudino <samuelcs131@gmail.com>
2021-08-20 21:03:47 -0300

    primeiro up

---
+++ js/toggle-botton.js
@@ -0,0 +1,19 @@
+ // BOTAO TOGGLE ATIVAR DESATIVAR USUARIO
+ const btnToggle = document.querySelector('.toggle-btn')
+ const toggleName = document.querySelector('.toggle-name')
+
+ btnToggle.onclick = event => {
+
+
+ btnToggle.classList.toggle('active-toggle')
+
+ if(btnToggle.classList == 'toggle-btn'){
+
+     toggleName.textContent = 'Ativado' 
+
+ }  else { 
+
+ toggleName.textContent = 'Desativado'
+
+}
+}
